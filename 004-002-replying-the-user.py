0403a2f70fa650e87353ca7a95f35dcfc396b935
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-14 17:45:59 -0300

    Create 004-002-replying-the-user.py

---
+++ 004-002-replying-the-user.py
@@ -0,0 +1,7 @@
+name = input('Por favor, digite o seu nome:')
+
+print('Olá, {}. Seja muito bem-vindo(a)!'.format(name))
+
+#algumas questão a considerar:
+#1. quero tirar os espaços que eventualmente a pessoa digitar ao escrever o nome (tanto antes quanto depois);
+#2. quero que o campo onde ela escreverá apareça na linha de baixo, e não ao lado da frase, no momento em que rodar o programa.
