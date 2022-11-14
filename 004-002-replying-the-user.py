713906de37bda84db608e8aaae6d1d63403043fa
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-14 17:43:30 -0300

    Delete 004-002-replying-the-user.py

--- 004-002-replying-the-user.py
+++
@@ -1,6 +0,0 @@
-name = input('Por favor, digite o seu nome:')
-print('Olá, {}. Seja muito bem-vindo(a)!'.format(name))
-#algumas questão a considerar:
-
-#1. quero tirar os espaços que eventualmente a pessoa digitar ao escrever o nome;
-#2. quero que o campo onde ela escreverá apareça na linha de baixo, e não ao lado da frase.
