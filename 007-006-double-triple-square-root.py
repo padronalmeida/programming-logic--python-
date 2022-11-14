f5cd7faf85a6cdb94363a26b4765eb908da53100
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-14 17:52:52 -0300

    Create 007-006-double-triple-square-root.py

---
+++ 007-006-double-triple-square-root.py
@@ -0,0 +1,3 @@
+n = int(input('Type a number of which you'd like to know the double, the triple and the square root:')
+
+print('The double of {} is {}, the triple is {} and the square root is {}.'.format(n, n*2, n*3, n**(1/2)))
