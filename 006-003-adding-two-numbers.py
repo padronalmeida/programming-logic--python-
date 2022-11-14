9d8292343f4609948b86024c346f8d00e9530e94
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-14 17:48:08 -0300

    Create 006-003-adding-two-numbers.py

---
+++ 006-003-adding-two-numbers.py
@@ -0,0 +1,7 @@
+print('Hello! I'll help you add two numbers of your choice!')
+
+n1 = int(input('Please, type the first number:'))
+n2 = int(input('Now, type the second number:'))
+sum = n1 + n2
+
+print('The sum of { } and {} equals {}.'.format(n1, n2, sum))
