f9f8796913acc72b48c747768f632198586d3dd1
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-15 11:27:57 -0300

    Create 007_008_measures_conversor.py

---
+++ 007_008_measures_conversor.py
@@ -0,0 +1,8 @@
+am = float(input('Digite uma medida (maior que zero) em metros:'))
+acm = am * 100
+amm = acm * 10
+
+print('{} metros equivale a {} centímetros e a {} milímetros.'.format(am, acm, amm))
+
+#melhorias: deixar com que a pessoa possa somente digitar números inteiros, deixando-a saber disso quando der...
+#... erro no programa.
