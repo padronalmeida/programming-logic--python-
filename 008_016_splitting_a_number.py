ae9a0556503581708d2655e09bd0560271ec0f35
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-17 13:02:47 -0300

    Create 008_016_splitting_a_number.py

---
+++ 008_016_splitting_a_number.py
@@ -0,0 +1,6 @@
+import math
+n = float(input('Digite um número real qualquer e obtenha somente sua parte inteira:'))
+print('O valor inteiro do número real digitado é {}.'.format(math.trunc(n)))
+
+# uma melhoria a esse programa seria conseguir definir quantas casas decimais truncar, o que pode ser...
+#... feito por uma função customizada como já vi e assim o farei assim que aprender como.
