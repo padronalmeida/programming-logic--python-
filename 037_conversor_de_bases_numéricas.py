b18790db710fcd57a3a640bcc691062e8c955096
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-21 15:54:14 -0300

    Create 037_conversor_de_bases_numéricas.py

---
+++ 037_conversor_de_bases_numéricas.py
@@ -0,0 +1,14 @@
+num = int(input('Digite um número inteiro qualquer:'))
+escolha = int(input("""
+Escolha para qual base quer converter o número digitado:
+1 - binário;
+2 - octal;
+3 - hexadecimal
+"""))
+
+if escolha == 1:
+    print('O número {} em binário é {}.'.format(escolha, bin(num)))
+elif escolha == 2:
+    print('O número {} em octal é {}.'.format(escolha, oct(num)))
+elif escolha == 3:
+    print('O número {} em hexadecimal é {}.'.format(escolha, hex(num)))
