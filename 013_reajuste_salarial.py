fbae42cf37e4411ca11496f376ed9659d0a1e3c9
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-21 14:21:51 -0300

    Create 013_reajuste_salarial.py

---
+++ 013_reajuste_salarial.py
@@ -0,0 +1,4 @@
+salario_atual = float(input('Digite o valor do salário atual (em reais):'))
+salario_reajuste_15 = salario_atual * 1.15
+
+print('O salário atual com reajuste de 15% fica R${:.0f}.'.format(salario_reajuste_15))
