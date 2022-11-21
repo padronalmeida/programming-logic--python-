d15b50c48b591be2c71d643f03b9af5f6a75bc15
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-21 14:34:52 -0300

    Update 015_aluguel_de_carros.py

--- 015_aluguel_de_carros.py
+++ 015_aluguel_de_carros.py
@@ -1,5 +1,7 @@
 km = float(input('Digite quantos kms foram percorridos com o carro:'))
 dias = int(input('Digite quantos dias ficou com o carro:'))
 valor = (60*dias) + (0.15*km)
-
-print('O valor do aluguel do carro será de R${:.2f}.'.format(valor))
+if (km <= 0) or (dias <= 0):
+    print('Tanto o número de dias quanto a kilometragem precisam ser positivas.')
+else:
+    print('O valor do aluguel do carro será de R${:.2f}.'.format(valor))
