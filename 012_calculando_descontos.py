fa84f9a139ffc6599b4f98fdae430eed4890a39e
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-21 14:19:12 -0300

    Create 012_calculando_descontos.py

---
+++ 012_calculando_descontos.py
@@ -0,0 +1,4 @@
+preco_original = float(input('Digite o preço original do produto:'))
+preco_desconto_5 = preco_original * 0.95
+
+print('O preço com desconto de 5% é R${:.0f}.'.format(preco_desconto_5))
