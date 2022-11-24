d2e133c91bdf65e90f4d20a3f6222d7b85880c23
rafael-padron <99772784+rafael-padron@users.noreply.github.com>
2022-11-24 09:37:24 -0300

    Create 038_comparando_numeros.py

---
+++ 038_comparando_numeros.py
@@ -0,0 +1,9 @@
+n1 = input("Digite o primeiro número:")
+n2 = input("DIgite o segundo número:")
+
+if n1 > n2:
+    print(f'{n1} é maior que {n2}.')
+elif n1 < n2:
+    print(f'{n2} é maior que {n1}.')
+else:
+    print('Os dois números são iguais.')
