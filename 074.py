83e16432a4853b89830a5284870c0b798b8ff6b9
Rafael Padron <r.padronalmeida@gmail.com>
2023-01-11 10:05:42 -0300

    criação 074

---
+++ 074.py
@@ -0,0 +1,7 @@
+import random
+
+tupla = (random.randint(1,100), random.randint(1,100), random.randint(1,100), random.randint(1,100), random.randint(1,100), )
+
+print(tupla)
+print(min(tupla))
+print(max(tupla))
